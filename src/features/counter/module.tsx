f70e75c11768836e6c3ca56ec20ef41162ff9007
mtnori <moto44.noriyuki@gmail.com>
2021-08-22 10:21:37 +0900

    add counter module.

---
+++ src/features/counter/module.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import * as Rx from 'typeless/rx';
+import { CounterActions, CounterState, useModule } from "./interface";
+import { Counter } from './components/Counter';
+
+useModule
+  .epic()
+  .on(CounterActions.startCount, () =>
+    Rx.of(CounterActions.countDone(1)).pipe(Rx.delay(500))
+  );
+
+const initialState: CounterState = {
+  isLoading: false,
+  count: 0
+};
+
+useModule
+  .reducer(initialState)
+  .on(CounterActions.startCount, state => {
+    state.isLoading = true;
+  })
+  .on(CounterActions.countDone, (state, { count }) => {
+    state.isLoading = false;
+    state.count += count;
+  });
+
+const CounterModule = () => {
+  useModule();
+  return (
+    <Counter />
+  );
+};
+export default CounterModule;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { DefaultTypelessProvider } from 'typeless';
 import { SubComponent } from 'components/sub-component';
+import CounterModule from 'features/counter/module';
 
 const App = () => (
   <div>
     <h1>Hello React!</h1>
     <SubComponent name="My Counter for TypeScript" />
+    <CounterModule />
   </div>
 );
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+ReactDOM.render(
+  <DefaultTypelessProvider>
+    <App />
+  </DefaultTypelessProvider>,
+  document.querySelector('#app'));
